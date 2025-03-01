3b73b90e2cbc5a528b6838d97feb85b4f4fa6e81
DuongThaiTung <pixelsunflowerstudio@gmail.com>
2025-03-01 20:45:23 +0700

    Add Event Manager Scripts

---
+++ Event Manager/EventManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class EventManager
+{
+	static readonly Dictionary<string, UnityEvent<object>> _events = new();
+	static readonly Dictionary<string, object> _eventData = new();
+
+	public static void StartListening(string eventName, UnityAction<object> callback)
+	{
+		if (_events.TryGetValue(eventName, out UnityEvent<object> thisEvent))
+		{
+			thisEvent.AddListener(callback);
+		}
+		else
+		{
+			thisEvent = new UnityEvent<object>();
+			thisEvent.AddListener(callback);
+
+			_events.Add(eventName, thisEvent);
+		}
+	}
+
+	public static void StopListening(string eventName, UnityAction<object> callback)
+	{
+		if (_events.TryGetValue(eventName, out UnityEvent<object> thisEvent))
+		{
+			thisEvent.RemoveListener(callback);
+		}
+	}
+
+	public static void EmitEvent(string eventName, object data = null)
+	{
+		if (_eventData.ContainsKey(eventName))
+		{
+			_eventData[eventName] = data;
+		}
+		else
+		{
+			_eventData.Add(eventName, data);
+		}
+
+		if (_events.TryGetValue(eventName, out UnityEvent<object> thisEvent))
+		{
+			thisEvent.Invoke(data);
+		}
+	}
+
+	public static T GetData<T>(string eventName)
+	{
+		if (_eventData.TryGetValue(eventName, out object data))
+		{
+			return (T)data;
+		}
+		return default;
+	}
+}
---
+++ Event Manager/EventName.cs
@@ -0,0 +1,8 @@
+﻿public class EventName
+{
+	// Define your own event IDs
+	public class Test
+	{
+		public const string TestEvent = "Test Event";
+	}
+}
